327f0a1b775d7bc0ae77a33182ffad11df7e9c8e
trojash <tonyestuardor@outlook.com>
2023-01-03 14:44:36 +0000

    Update Calculator

--- CalculatorApplication/Program.cs
+++ CalculatorApplication/Program.cs
@@ -1,14 +1,14 @@
-﻿int num1 = 0; 
-int num2 = 0;
+﻿double num1 = 0; 
+double num2 = 0;
 
 Console.WriteLine("Console Calculator in C#\r");
 Console.WriteLine("--------------------------");
 
 Console.WriteLine("Type a number, and then press Enter");
-num1 = Convert.ToInt32(Console.ReadLine());
+num1 = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine("Type a number, and then press Enter");
-num2 = Convert.ToInt32(Console.ReadLine());
+num2 = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine("Choose an option from the following list:");
 Console.WriteLine("\ta - Add");
@@ -17,7 +17,7 @@ Console.WriteLine("\tm - Multiply");
 Console.WriteLine("\td - Divide");
 Console.Write("Your option? ");
 string choice = Console.ReadLine();
-//int choice = Convert.ToIn32(Console.ReadLine());
+//double choice = Convert.ToIn32(Console.ReadLine());
 
 switch (choice)
 {
