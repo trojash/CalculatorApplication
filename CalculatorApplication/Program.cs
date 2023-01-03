8ea2244dadd81fbed18167279d7a75d908bf9834
trojash <tonyestuardor@outlook.com>
2023-01-03 14:29:46 +0000

    First Release

---
+++ CalculatorApplication/Program.cs
@@ -0,0 +1,40 @@
+﻿int num1 = 0; 
+int num2 = 0;
+
+Console.WriteLine("Console Calculator in C#\r");
+Console.WriteLine("--------------------------");
+
+Console.WriteLine("Type a number, and then press Enter");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Type a number, and then press Enter");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Choose an option from the following list:");
+Console.WriteLine("\ta - Add");
+Console.WriteLine("\ts - Subtract");
+Console.WriteLine("\tm - Multiply");
+Console.WriteLine("\td - Divide");
+Console.Write("Your option? ");
+string choice = Console.ReadLine();
+//int choice = Convert.ToIn32(Console.ReadLine());
+
+switch (choice)
+{
+	//case 1:
+		//break;
+	case "a":
+		Console.WriteLine($"Your result: {num1} + {num2} = " + ((num1 + num2)));
+		break; 
+	case "s":
+        Console.WriteLine($"Your result: {num1} - {num2} = " + ((num1 - num2)));
+        break; 
+	case "m":
+        Console.WriteLine($"Your result: {num1} * {num2} = " + ((num1 * num2)));
+        break; 
+	case "d":
+        Console.WriteLine($"Your result: {num1} / {num2} = " + ((num1 / num2)));
+        break;
+	default:
+		break;
+}
